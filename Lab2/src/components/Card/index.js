51b66a5156e9023488458b12ffb683f8e88b3f15
Dykota <43105455+Dykota@users.noreply.github.com>
2019-09-28 17:08:39 -0400

    Changes

---
+++ Lab2/src/components/Card/index.js
@@ -0,0 +1,15 @@
+import "./src/components/Card/index.css";
+
+class Card {
+
+  constructor(props) {
+    this.props = props;
+  }
+
+  render() {
+    `<h1 class="title">${this.props.title}</h1>`;
+  }
+
+}
+
+export default Card;
---
+++ Lab2/src/index.js
@@ -0,0 +1,6 @@
+import Element from './src/components/Element.js';
+import Card from './src/components/Card/index.js';
+
+let element = new Element({title: "New Title. Much excitement!"});
+
+console.log(element.render() );
